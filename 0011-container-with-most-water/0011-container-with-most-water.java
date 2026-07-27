e802236a983adcac11a0bb6662da7f1b3bec6ccf
Harman <officialharman69@gmail.com>
2026-07-27 20:54:57 +0530

    Time: 5 ms (82.83%), Space: 77.2 MB (86.09%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -23,3 +23,29 @@ class Solution {
         return maxArea;
     }
 }
+
+// class Solution {
+//     public int maxArea(int[] height) {
+//         int n=height.length;
+//         int i=0;
+//         int j=n-1;
+//         int w=0;
+//         int h=0;
+//         int ans=Integer.MIN_VALUE;
+//         int curr=0;
+//         while(i<j)
+//         {
+//             w=j-i;
+//             h=Math.min(height[i],height[j]);
+//             curr=w*h;
+//             ans=Math.max(curr,ans);
+//             if(height[i]<=height[j])
+//             {
+//                 i++;
+//             }
+//             else
+//             j--;
+//         }
+//         return ans;
+//     }
+// }
