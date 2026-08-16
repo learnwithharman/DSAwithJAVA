f64d3d5290eae4acb5fd0a592e67f7370536d689
Harman <officialharman69@gmail.com>
2026-08-16 23:59:42 +0530

    Time: 0 ms (100%), Space: 45.7 MB (73.76%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.java
+++ 0832-flipping-an-image/0832-flipping-an-image.java
@@ -4,22 +4,16 @@ class Solution {
         for (int i = 0; i < image.length; i++) {
 
             // Step 1: Reverse the row
-            int start = 0;
-            int end = image[i].length - 1;
-
-            while (start <= end) {
-                int temp = image[i][start];
-                image[i][start] = image[i][end];
-                image[i][end] = temp;
-
+            int start =0;
+            int end = image[i].length-1;
+            while(start<=end){
+                int temp= image[i][start];
+                image[i][start]=1-image[i][end];
+                image[i][end]=1-temp;
                 start++;
                 end--;
             }
 
-            // Step 2: Invert 0 -> 1 and 1 -> 0
-            for (int j = 0; j < image[i].length; j++) {
-                image[i][j] = 1 - image[i][j];
-            }
         }
         return image;
     }
