d9501ff6db7b549824a093e5f7786c939dae0c1f
Harman <officialharman69@gmail.com>
2026-08-18 23:02:24 +0530

    Time: 0 ms (100%), Space: 46.5 MB (14.13%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -1,19 +1,11 @@
 class Solution {
     public int diagonalSum(int[][] mat) {
-
         int n = mat.length;
         int sum = 0;
-
         for (int i = 0; i < n; i++) {
-
-            // Primary diagonal
             sum += mat[i][i];
-
-            // Secondary diagonal
             sum += mat[i][n - 1 - i];
         }
-
-        // Center element was counted twice
         if (n % 2 == 1) {
             sum -= mat[n / 2][n / 2];
         }
