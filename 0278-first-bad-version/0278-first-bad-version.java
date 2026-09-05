d39250c4bfd765f21f78df8ba7f84c050f17af18
Harman <officialharman69@gmail.com>
2026-09-05 22:16:10 +0530

    Time: 14 ms (50.56%), Space: 41.8 MB (86.8%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -1,9 +1,6 @@
 /* The isBadVersion API is defined in the parent class VersionControl.
       boolean isBadVersion(int version); */
 
-/* The isBadVersion API is defined in the parent class VersionControl.
-      boolean isBadVersion(int version); */
-
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         
