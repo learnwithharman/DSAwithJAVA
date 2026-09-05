9f42645fcaaa20ffa40d6e233f8faa4af24b6e6d
Harman <officialharman69@gmail.com>
2026-09-05 22:15:44 +0530

    Time: 14 ms (50.56%), Space: 42.3 MB (17.27%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -1,24 +1,26 @@
 /* The isBadVersion API is defined in the parent class VersionControl.
       boolean isBadVersion(int version); */
 
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int start = 1;
-        int end = n;
-        while (start < end) {
-            int mid = start + (end - start) / 2;
-            if (isBadVersion(mid)) {
-                // mid bad hai
-                // first bad mid ya uske left mein ho sakta hai
-                end = mid;
+        
+        int left = 1;
+        int right = n;
+        int mid;
+        while (left < right){
+            mid = left + (right - left)/2;
 
-            } else {
+            boolean isBad = isBadVersion(mid);
 
-                // mid good hai
-                // first bad definitely right mein hai
-                start = mid + 1;
+            if (isBad){
+                right = mid;
+            }else{
+                left = mid + 1;
             }
         }
-        return start;
+        return left;
     }
 }
