506cd21e581235e9a672d57066656fb4e7b96f32
Harman <officialharman69@gmail.com>
2026-09-18 17:24:00 +0530

    Time: 3 ms (69.93%), Space: 47.7 MB (48.25%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int i=0;
+        while(i<nums.length){
+            int correct = nums[i]-1;
+            if(nums[i]!=nums[correct]){
+                int temp=nums[i];
+                nums[i]=nums[correct];
+                nums[correct]=temp;
+            }else i++;
+        }
+        int[] arr= new int [2];
+        for (int j=0;j<nums.length;j++){
+            if(nums[j] != j + 1){
+                arr[0]=nums[j];
+                arr[1]=j+1;
+            }
+        }
+        return arr;
+    }
+}
